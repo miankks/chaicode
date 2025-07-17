5fcaa10ec481cb0a6340ed4358a16b995e70d4a1
Sweden <techbilal85@gmail.com>
2025-07-17 08:27:51 +0100

    hooks introduction

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Counter = () => {
-    let counter = 5;
+    let [counter, setCounter] = useState(5);
 
     const addValue = () => {
-        counter = counter +1;
-        console.log(counter);
+        counter = counter +1
+        counter >= 6 ? alert('Can not increase above zero') : setCounter(counter)        
+    }
+
+    const removeValue = () => {
+        counter = counter -1;
+        counter < 0 ? alert('Can not decrease below zero') : setCounter(counter)
         
     }
 
@@ -14,7 +19,7 @@ const Counter = () => {
         <h2>Counter value: {counter}</h2>
         <button onClick={addValue}>Increase value {counter}</button>
         <br />
-        <button>Decrease value {counter}</button>
+        <button onClick={removeValue}>Decrease value {counter}</button>
         <p>{counter}</p>
     </div>
   )
